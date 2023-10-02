d73bac19c6ad5eb944f5fe06b87d7c577f2b1858
Denis Bunak <xisi926@ukr.net>
2023-10-02 22:11:30 +0300

    Pilot

---
+++ src/global/goit/edu/module13/habr/HabrParseTests.java
@@ -0,0 +1,37 @@
+package global.goit.edu.module13.habr;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HabrParseTests {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String url = "https://habr.com/ru/articles/";
+
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create(url))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpClient client = HttpClient
+                .newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        String bodyHabr = response.body();
+
+        FileWriter writer = new FileWriter("habr-new.html");
+        writer.write(bodyHabr);
+        writer.close();
+
+    }
+}
